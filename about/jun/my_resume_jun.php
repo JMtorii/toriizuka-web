f7fb83504b6faa147f81c50dc7c13a11ba3916bc
Jun Mark Torii <j.torii4@gmail.com>
2014-08-26 19:34:20 -0400

    Better resume layout for Jun

--- about/jun/my_resume_jun.php
+++ about/jun/my_resume_jun.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html public "Everything is made by Jun Torii">
-
+<html style="height:100%;">
 <head>
 	<?php include '../../helpers/head_content.php';?>
 </head>
 
-<body>
-	<section id="pdf-section">
-		<div id="pdf-area">
-			<object data="Jun_Mark_Torii-resume.pdf" type="application/pdf"></object>
-		</div>
-	</section>
+<body style="overflow:hidden; width:100%; height:100%">
+<!-- 	<section id="pdf-section" style="overflow:hidden;">
+		<div id="pdf-area" style="overflow:hidden;"> -->
+			<object data="Jun_Mark_Torii-resume.pdf" type="application/pdf" style="width:inherit; height:inherit;"></object>
+<!-- 		</div>
+	</section> -->
 
 </body>
 
